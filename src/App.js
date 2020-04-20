55d5a1ab8e7568f203cacccdeb384af71c17f238
Matheus Silva Freitas <msformigteen@live.com>
2020-04-20 01:06:49 -0300

    Atualiza o Git Ignore

--- src/App.js
+++ src/App.js
@@ -31,9 +31,11 @@ function App() {
               }}
           >
               <Box 
-                  maxWidth='50vw' 
                   px={4} 
                   flex={1}
+                  sx={{
+                      minWidth: '300px'
+                  }}
               >
                   <Heading 
                       as='h1' 
@@ -45,22 +47,26 @@ function App() {
                   </Heading>
               </Box>
               <Box 
-                  px={2}
+                  px={5}
                   flex={1}
-                  maxWidth='50vw' 
+                  sx={{
+                      justifyContent: 'center',
+                      minWidth: '300px',
+                      alignItems: 'center'
+                  }}
               >
                   <Box 
+                      m='auto'
                       sx={{ 
-                          height: '50vh',
-                          width: '50vh'
+                          maxHeight: '50vh',
+                          maxWidth: '50vh'
                       }}
                   >
                       <Image
-                      sx={{ 
-                          objectFit: 'cover',
-                          borderRadius: '100%',
-                          width: '50vh'
-                      }}
+                          sx={{ 
+                              objectFit: 'cover',
+                              borderRadius: '100%'
+                          }}
 
                           src='https://lh3.googleusercontent.com/CfqqxquiMoqlktUK3IQKbkNH2zThS55Lwy2si1eS3sXAVGJZp1xDiuFQX8N69Xx8uzfyzHnX4dNvDzuC1Srf-_k6SFjrBUq60SElLQdMPUYlPzkwvFkGyWZ7J2jExgi_NWB16xh3t5K9wQsj_xuyUYPLjj2-REPYX47GsWAwy7KGVTy-2T2hfuvB9jfVShwWP7I1ireNea0Mm0GMDT9vi0RVRenp6RHeWhZZDPLFsXe502xKxwTYJ5JS6N8_LahKHCxt85b5_dPf2Jnb1GmghVtPxZzz0XDqcNB4aywcd3HVlqsj35O14xdpC7mer1Z9SB9UzOSnpyCYzSez3zPo4CZ75ugEawOxMz8h6PI42X6F9PblqEyz71ZdX7FqXZ6vbEB0rmDypF8Zui_Wb1N5WNQ_HN62mU39SBRO4MqS7r-LDrF5QQWeWzNGtl3Rlsg0Abc1Cs-ou3rovUzJiKFMC11KHoeoiPAfghYVht5OmgCWWFsLMf8367HTQfcYcLi4SNooJ4K4-pl_8ThLi8UEso2bNT-a_epkJErtNzVbwMsotsZ_o9HmYwa2HtbijhRa1M7yo5u2eibbYh5ku5BHQ9LnkGxBdo7RGdX_rhisVC5RUxXkSic6SZ_z0u-pnvhmjDw6ClVeKdTwry0xWMPRvlwhPUJkMIaOXGNEu-m9cxTzxKi7QOeZu8e91SpQ2g=s907-no'
                       />
