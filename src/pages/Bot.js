83f6d5aaa41def8b9aa89cb6118822fe4ac85e38
Matheus Silva Freitas <msformigteen@live.com>
2020-08-14 22:58:28 -0300

    Implement Animation Typing

--- src/pages/Bot.js
+++ src/pages/Bot.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { 
     Heading,
     Flex,
     Box
 } from 'rebass/styled-components'
-import Typing from 'react-typing-animation';
+import Typist from 'react-typist';
 import { Input } from '@rebass/forms'
+import "react-typist/dist/Typist.css"
 
 
-const Bot = () => (
-    <Flex
-        bg='primary'
-        flexDirection='column'
-        px={[2, 3, 3, 5]}
-        py={[5, 5, 5, 6]}
-        sx={{
-            justifyContent: 'center',
-            alignItems: 'center',
-        }}
-    >
-        <Box 
-            mb={[3, 3, 3, 4]}
-            flex={0}>
-            <Typing cursorClassName="cursor-white">
+const Bot = () => {
+    const [message, setMessage] = useState("Hi!")
+    const [text, setText] = useState("")
+    const handleChange = (event) => {
+        const { target: { value } } = event;
+        setText(value);
+    }
+    const handleKeyUp = ({ keyCode }) => {
+        if ( keyCode === 13 ) {
+            setMessage(text || "*u*")
+            setText("")
+        }
+    }
+    return (
+        <Flex
+            bg='primary'
+            flexDirection='column'
+            px={[2, 3, 3, 5]}
+            py={[5, 5, 5, 6]}
+            sx={{
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            <Box 
+                mb={[3, 3, 3, 4]}
+                flex={0}>
                 <Heading 
                     as='h2' 
                     mb={[4, 4, 4, 5]}
@@ -31,35 +44,40 @@ const Bot = () => (
                         textAlign: 'center',
                     }}
                     color='secondary'>
-                    Hi!
+                    <Typist key={message}>
+                        { message }
+                    </Typist>
                 </Heading>
-            </Typing>
-        </Box>
-        <Box 
-            px={5}
-            sx={{
-                width: '100%',
-            }}
-            flex={1}
-        >
-            <Input
-                name='message'
-                type='text'
-                fontSize={[4, 4, 4, 4]}
+            </Box>
+            <Box 
+                px={5}
                 sx={{
-                    color: '#E7E4EF',     
-                    textAlign: 'center',
-                    border: '0px',
-                    ':focus': {
-                        outline: 'none',
-                        borderBottom: '1px solid',
-                        color: '#E7E4EF',     
-                    }
+                    width: '100%',
                 }}
-                placeholder='Type something'
-            />
-        </Box>
-    </Flex>
-)
+                flex={1}
+            >
+                <Input
+                    name='message'
+                    type='text'
+                    fontSize={[4, 4, 4, 4]}
+                    value ={text}
+                    onChange={handleChange} 
+                    onKeyUp={handleKeyUp}
+                    sx={{
+                        color: '#E7E4EF',     
+                        textAlign: 'center',
+                        border: '0px',
+                        ':focus': {
+                            outline: 'none',
+                            borderBottom: '1px solid',
+                            color: '#E7E4EF',     
+                        }
+                    }}
+                    placeholder='Type something'
+                />
+            </Box>
+        </Flex>
+    )
+}
 
 export default Bot
